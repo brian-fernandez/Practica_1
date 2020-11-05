42192947fed4cf9265ca81c556a7dd9a2ac90efe
brian <nsklobuchar666@gmail.com>
2020-11-05 02:08:00 -0400

    hj

---
+++ pizza/js/java.js
@@ -0,0 +1,68 @@
+function openMenu() {
+    var navMain = document.getElementById('navMenuId');
+    if (navMain.className === 'navMenu') {
+        navMain.className += ' mobileView';
+    } else {
+        navMain.className = 'navMenu';
+    }
+}
+
+
+function pizza()
+{
+    var contenido1 = document.getElementById("parrafo-1");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+function pizza2()
+{
+    var contenido1 = document.getElementById("parrafo-2");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+function pizza3()
+{
+    var contenido1 = document.getElementById("parrafo-3");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+
+
+var cont1 = document.getElementById("cont1");
+cont1.style.display="none";
+var cont2 = document.getElementById("cont2");
+cont2.style.display="none";
+
+
+
+
+
+
+function activado1()
+{
+    const dato6 =document.querySelector(".cont-1");
+    
+    
+    
+        
+    if(cont2.style.display="none")
+    {
+        dato6.classList.toggle("active");
+        cont2.style.display=="none";
+        cont1.style.display=="block";
+       
+    }
+    
+
+
+}
+
+function activado2()
+{
+    const dato7 =document.querySelector(".cont-2");
+    dato7.classList.toggle("active");
+
+    var cont1 = document.getElementById("cont1");
+    if(
+
+    )
+
+
+}
