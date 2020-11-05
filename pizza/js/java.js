2cc4291bcacaf5c7b780fd5d9d6d46cc46f03f8a
brian <nsklobuchar666@gmail.com>
2020-11-05 14:57:05 -0400

    G

--- pizza/js/java.js
+++ pizza/js/java.js
@@ -25,6 +25,27 @@ function pizza3()
 }
 
 
+
+
+function hamburger()
+{
+    var contenido1 = document.getElementById("parrafo-1-1");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+function hamburger2()
+{
+    var contenido1 = document.getElementById("parrafo-2-2");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+function hamburger3()
+{
+    var contenido1 = document.getElementById("parrafo-3-3");
+    contenido1.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eligendivoluptatem officia nam, perspiciatis reiciendis ad at explicabo ratione, modi voluptates suscipitreprehenderit rerum. Ea consectetur animi tempore eos aspernatur!"
+}
+
+
+
+
 function openventana(cityName) {
     var i;
     var x = document.getElementsByClassName("city");
