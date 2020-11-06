e1e457279b5ed6bb9f358474d98ed60824f31ad3
brian <nsklobuchar666@gmail.com>
2020-11-06 16:25:59 -0400

    xcv

--- js/java.js
+++ js/java.js
@@ -46,11 +46,11 @@ function hamburger3()
 
 
 
-function openventana(cityName) {
+function openventana(Name) {
     var i;
     var x = document.getElementsByClassName("city");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
-    document.getElementById(cityName).style.display = "block";  
+    document.getElementById(Name).style.display = "block";  
   }
